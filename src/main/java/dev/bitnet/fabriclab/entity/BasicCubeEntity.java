d1ff49fc1684bfc957e427fa31f676b094d15ef6
BitNet <thomasbeddows@gmail.com>
2020-07-31 14:44:58 +0100

    Added ClientInitializer to fabric.mod.json to fix renderer problem

--- src/main/java/dev/bitnet/fabriclab/entity/BasicCubeEntity.java
+++ src/main/java/dev/bitnet/fabriclab/entity/BasicCubeEntity.java
@@ -1,18 +1,11 @@
 package dev.bitnet.fabriclab.entity;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.world.World;
 
-public class BasicCubeEntity extends AnimalEntity {
-
-    protected BasicCubeEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+public class BasicCubeEntity extends MobEntity {
+    public BasicCubeEntity(EntityType<? extends MobEntity> entityType, World world) {
         super(entityType, world);
-    }
-
-    @Override
-    public PassiveEntity createChild(PassiveEntity mate) {
-        return null;
     }
 }
